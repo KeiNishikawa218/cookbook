ceb36e97211d2eba0b2253edbff415bbf70470ba
k-nishikawa <k-nishikawa@atware.co.jp>
2021-08-05 12:46:48 +0900

    finish chapter 10

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -7,6 +7,7 @@ import { RECIPEDATA } from './recipe/recipedata';
 })
 export class RecipeService {
   recipedata: Recipe[] = RECIPEDATA;
+  currentValue: string = '';
 
   constructor() { }
 
@@ -16,5 +17,17 @@ export class RecipeService {
 
   getRecipe(id:string): Recipe{
     return this.recipedata.find(recipe => recipe.id!.toString()==id) as Recipe;
+  }
+
+  searchRecipe(keyword: string): Recipe[] {
+    let resultArr: Recipe[] = [];
+
+    for (let recipe of this.recipedata) {
+      let dataStr = JSON.stringify(recipe);
+      if (dataStr.search(keyword) >= 0) {
+        resultArr.push(recipe);
+      }
+    }
+    return resultArr;
   }
 }
