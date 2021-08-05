474da4b48a2b2e15b96541f6142b7b06ab418411
k-nishikawa <k-nishikawa@atware.co.jp>
2021-08-05 11:57:05 +0900

    update: finish 10-2

--- src/app/recipe-data/recipe-data.component.ts
+++ src/app/recipe-data/recipe-data.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router'
 import { Location } from '@angular/common';
 
 import { Recipe } from '../recipe/recipe';
-import { RECIPEDATA } from '../recipe/recipedata';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-data',
@@ -14,11 +14,11 @@ export class RecipeDataComponent implements OnInit {
   recipeid?: string;
   recipe?: Recipe;
 
-  constructor(private route: ActivatedRoute, private location: Location) { }
+  constructor(private route: ActivatedRoute, private location: Location, private rsv: RecipeService) { }
 
   ngOnInit(): void {
     this.recipeid = this.route.snapshot.paramMap.get('id') || '1';
-    this.recipe = RECIPEDATA.find(recipe => recipe.id?.toString() == this.recipeid);
+    this.recipe = this.rsv.getRecipe(this.recipeid);
   }
 
   backToList() {
